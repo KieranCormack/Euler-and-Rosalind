1cc07022b4448189cf86a006e8f5ef060c1e7fbf
Kieran Cormack <kieran.cormack@unbounce.com>
2015-05-04 10:12:36 -0700

    4,5,6

--- rosalind_ex4.rb
+++ rosalind_ex4.rb
@@ -1,14 +1,6 @@
 def hamm(filepath)
 	array = File.readlines(filepath).map { |line| line.strip.split('') }
-	arr_one = array[0]
-	arr_two = array[1]
-
-	merge = arr_one.zip(arr_two)
-
-	mistakes = merge.count { |x, y| x != y }
-
-	print mistakes
-
+	array[0].zip(array[1]).count { |x,y| x != y }
 end
 
 puts hamm('hamm.txt')
